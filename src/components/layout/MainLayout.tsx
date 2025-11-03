88ce669f6c7abec685279fe33130e8b6398911c4
MdShijanAli <shijan135@gmail.com>
2025-11-03 12:52:43 +0600

    style: Standardize quotation marks and improve formatting in MainLayout component

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,8 +1,8 @@
-import { Outlet } from 'react-router-dom';
-import Sidebar from './Sidebar';
-import Header from './Header';
-import { useSidebarStore } from '@/stores/sidebarStore';
-import { cn } from '@/lib/utils';
+import { Outlet } from "react-router-dom";
+import Sidebar from "./Sidebar";
+import Header from "./Header";
+import { useSidebarStore } from "@/stores/sidebarStore";
+import { cn } from "@/lib/utils";
 
 export default function MainLayout() {
   const { isCollapsed } = useSidebarStore();
@@ -12,12 +12,12 @@ export default function MainLayout() {
       <Sidebar />
       <div
         className={cn(
-          'transition-all duration-300',
-          isCollapsed ? 'ml-16' : 'ml-64'
+          "transition-all duration-300",
+          isCollapsed ? "ml-16" : "ml-64"
         )}
       >
         <Header />
-        <main className="p-6">
+        <main className={`${isCollapsed ? "p-6" : "p-3"}`}>
           <Outlet />
         </main>
       </div>
