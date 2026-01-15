69094d4e2133238cdd6d01a406deb9f61b74ead3
MdShijanAli <shijan135@gmail.com>
2026-01-15 13:54:31 +0600

    refactor: remove unused translation hooks and simplify logo rendering in Login and ForgotPassword components

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from "react";
+import React, { lazy } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import PrivateRoute from "./PrivateRoute";
 import PublicRoute from "./PublicRoute";
